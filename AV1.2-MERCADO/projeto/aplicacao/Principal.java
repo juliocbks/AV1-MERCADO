bf52d441014db31d84239ea40c0a1e7e84e6d25d
Júlio César <131193517+juliocbks@users.noreply.github.com>
2023-06-09 17:22:01 -0300

    Update Principal.java

--- AV1.2-MERCADO/projeto/aplicacao/Principal.java
+++ AV1.2-MERCADO/projeto/aplicacao/Principal.java
@@ -2,15 +2,13 @@ package projeto.aplicacao;
 
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.io.*;
  
 import projeto.entidades.Produto;
 import projeto.entidades.ProdutosAlimenticios;
 import projeto.entidades.ProdutosEletronicos;
 import projeto.entidades.ProdutosVestuario;
-import projeto.entidades.Categorias;
 import projeto.entidades.InicializadorProdutos;
-
+import projeto.entidades.Cores;
 
 public class Principal {
     public static void main(String[] args) {
@@ -24,11 +22,12 @@ public class Principal {
         ArrayList<Produto> listaProdutos = new ArrayList<Produto>();
         Scanner sc = new Scanner(System.in);
 
-        // Inicialização dos objetos pré-definidos e categorias:
+        // Inicialização dos objetos pré-definidos:
         InicializadorProdutos.inicializarProdutos(listaProdutos);
 
         // Loop para o menu:
         while (opcao != 0) {
+
             // Menu:
             do {
                 System.out.print("\n|-------------<<<|MENU|>>>-------------|\n");
@@ -40,45 +39,47 @@ public class Principal {
                 System.out.print("| Opção 6 - Remover produto            |\n");
                 System.out.print("| Opção 0 - Encerrar programa     <<<--|\n");
                 System.out.print("|---------------------->>>\n");
-                System.out.print("|-------->>> Digite uma opção: ");
+                System.out.print("|-------->>> "+Cores.AMARELO+Cores.AMARELO_SUBLINHADO+"Digite uma opção"+Cores.RESET+": ");
                 if (sc.hasNextByte()) {
                     opcao = sc.nextByte();
-                    // sc.nextLine();
                     break;
                 } else {
-                    System.out.println("\n*** Informe uma opção válida! ***");
-                    sc.next();
+                    System.out.println(Cores.VERMELHO+"\n*** Informe uma opção válida! ***"+Cores.RESET);
                 }
                 sc.next();
             } while (true);
 
             // Switch case para funcionalidade do menu:
             switch (opcao) {
+                // Listar todos os produtos, NOME | CATEGORIA | ID:
                 case 1:
                     produto.listarProdutos(listaProdutos);
                     break;
+                // Listar todos os produtos de x categoria:
                 case 2:
                     produto.filtrarCategoria(listaProdutos);
                     break;
+                // Listar todos os atributos de um produto em específico:
                 case 3:
                 produto.listarProdutos(listaProdutos);
-                System.out.print("\n-> Digite o ID do produto que você deseja visualizar: ");
+                System.out.print("\n>>> "+Cores.AMARELO+Cores.AMARELO_SUBLINHADO+"Digite o ID do produto que você deseja visualizar"+Cores.RESET+": ");
                 do{
                     if(sc.hasNextByte()) {
                         id = sc.nextByte();
                         produto.visualizarProduto(listaProdutos, id);
                         break;
                     } else {
-                        System.out.println("\n*** Valor informado é inválido, por favor, digite novamente um valor numérico válido! ***");
-                        System.out.print("\n-> Digite o ID do produto que você deseja visualizar: ");
+                        System.out.println(Cores.VERMELHO+"\n*** Valor informado é inválido, por favor, digite novamente um valor numérico válido! ***"+Cores.RESET);
+                        System.out.print("\n>>> "+Cores.AMARELO+Cores.AMARELO_SUBLINHADO+"Digite o ID do produto que você deseja visualizar"+Cores.RESET+": ");
                         sc.next();
                     }                
                 } while(true);
                 break;
+                // Cadastrar um novo produto em uma das 3 categorias:
                 case 4:
                     System.out.println("\n-----<<<|TIPOS DE PRODUTO|>>>----- \n 1 >> Produto alimentício\n 2 >> Produto eletrônico \n 3 >> Produto de vestuário");
                     do{
-                        System.out.print("\nInsira o tipo de produto a cadastrar: ");
+                        System.out.print("\n>>> "+Cores.AMARELO+Cores.AMARELO_SUBLINHADO+"Insira o tipo de produto a cadastrar: "+Cores.RESET);
                         if(sc.hasNextByte()) {
                             byte tipo = sc.nextByte();
                             switch(tipo) {
@@ -92,18 +93,19 @@ public class Principal {
                                 produto.cadastrarProdutoVestuario(listaProdutos);
                                 break;
                                 default:
-                                System.out.println("*** Por favor, insira um valor dentro dos parâmetros! ***");
+                                System.out.println(Cores.VERMELHO+"*** Por favor, insira um valor dentro dos parâmetros! ***"+Cores.RESET);
                             }
                             break;
                         } else {
-                            System.out.println("\n*** Valor informado é inválido, por favor, digite novamente um valor numérico válido! ***\n");
+                            System.out.println(Cores.VERMELHO+"\n*** Valor informado é inválido, por favor, digite novamente um valor numérico válido! ***\n"+Cores.RESET);
                             sc.next();
                         }
                     } while(true);
                     break;
+                // Editar um produto pelo seu ID:
                 case 5:
                 produto.listarProdutos(listaProdutos);
-                System.out.print("\n-> Digite o ID do produto que você deseja editar: ");
+                System.out.print("\n>>> "+Cores.AMARELO+Cores.AMARELO_SUBLINHADO+"Digite o ID do produto que você deseja editar"+Cores.RESET+": ");
                 if (sc.hasNextByte()) {
                     byte idProduto = sc.nextByte();
                     boolean produtoEncontrado = false;
@@ -117,39 +119,41 @@ public class Principal {
                             } else if (p instanceof ProdutosVestuario) {
                                 ((ProdutosVestuario) p).editarProdutoVestuario(listaProdutos, idProduto);
                             } else {
-                                System.out.println("\n*** Categoria do produto desconhecida! ***");
+                                System.out.println(Cores.VERMELHO+"\n*** Categoria do produto desconhecida! ***"+Cores.RESET);
                             }
                             break;
                         }
                     }
                     if (!produtoEncontrado) {
-                        System.out.println("\n*** Produto não encontrado! ***");
+                        System.out.println(Cores.VERMELHO+"\n*** Produto não encontrado! ***"+Cores.RESET);
                     }
                 } else {
-                    System.out.println("\n*** Valor informado é inválido, por favor, digite novamente um valor numérico válido! ***");
+                    System.out.println(Cores.VERMELHO+"\n*** Valor informado é inválido, por favor, digite novamente um valor numérico válido! ***"+Cores.RESET);
                     sc.next();
                 }
                 break;
+                // Remover um produto pelo seu ID:
                 case 6:
                     do{
                         produto.listarProdutos(listaProdutos);
-                        System.out.print("\n-> Digite o ID do produto que você deseja remover: ");
+                        System.out.print("\n>>> "+Cores.AMARELO+Cores.AMARELO_SUBLINHADO+"Digite o ID do produto que você deseja remover"+Cores.RESET+": ");
                         if(sc.hasNextByte()) {
                         id = sc.nextByte();
                         produto.removerProduto(listaProdutos, id);
                         break;
                         } else {
-                            System.out.println("\n*** Valor informado é inválido, por favor, digite novamente um valor numérico válido! ***");
+                            System.out.println(Cores.VERMELHO+"\n*** Valor informado é inválido, por favor, digite novamente um valor numérico válido! ***"+Cores.RESET);
                             sc.next();
                         }
                     } while(true);
                     break;
+                // Encerrar programa:
                 case 0:
-                    System.out.println("\n*** Programa encerrado! ***");
+                    System.out.println(Cores.VERMELHO+"\n*** Programa encerrado! ***"+Cores.RESET);
                     sc.close();
                     break;
                 default:
-                    System.out.println("\n*** Insira uma opção válida! ***");
+                    System.out.println(Cores.VERMELHO+"\n*** Insira uma opção válida! ***"+Cores.RESET);
                     break;
             }
         }

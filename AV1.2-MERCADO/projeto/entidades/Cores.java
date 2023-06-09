906b73b3ee6106d7174290c1cca049606b4635ae
Júlio César <131193517+juliocbks@users.noreply.github.com>
2023-06-09 17:25:05 -0300

    Add files via upload

---
+++ AV1.2-MERCADO/projeto/entidades/Cores.java
@@ -0,0 +1,10 @@
+package projeto.entidades;
+
+// Cores para as mensagens do terminal:
+public class Cores {
+    public static final String RESET = "\u001B[0m";
+    public static final String VERMELHO = "\u001B[31m";
+    public static final String AMARELO = "\u001B[33m";
+    public static final String AMARELO_SUBLINHADO = "\033[4;33m";
+    public static final String VERDE = "\u001B[32m";
+}
